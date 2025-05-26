1fabfb88910d2144fbb4712b551ccc879f5d1516
link200309 <jonhh.0023@gmail.com>
2025-05-26 00:27:36 -0400

    feat(dbpedia): Query for get Products

--- services/dbpedia.py
+++ services/dbpedia.py
@@ -29,8 +29,56 @@ def consultar_dbpedia(termino, idioma):
 
     try:
         resultados = sparql.query().convert()
+
+        print(resultados)
+        
         if resultados["results"]["bindings"]:
             return resultados["results"]["bindings"][0]["abstract"]["value"]
     except Exception as e:
         print("Error DBpedia:", e)
     return "No results found in DBpedia."
+
+
+#Esta funcion simplemente es para obtener todos los productos que estan en
+def obtener_productos_tipo_cake():
+    sparql = SPARQLWrapper("https://dbpedia.org/sparql")
+    sparql.setReturnFormat(JSON)
+
+    query = """
+    PREFIX dbo: <http://dbpedia.org/ontology/>
+    PREFIX dbp: <http://dbpedia.org/property/>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+    SELECT ?producto ?nombre ?sabor ?tipo ?lang WHERE {
+      ?producto rdf:type dbo:Food ;
+                rdfs:label ?nombre ;
+                dbo:type ?tipo .
+      OPTIONAL { ?producto dbp:flavor ?sabor . }
+      BIND (lang(?nombre) AS ?lang)
+      FILTER (?lang = "en" || ?lang = "es")
+      FILTER CONTAINS(LCASE(STR(?tipo)), "cake")
+    }
+    LIMIT 1000
+    """
+
+    sparql.setQuery(query)
+
+    try:
+        resultados = sparql.query().convert()
+        productos = []
+
+        for r in resultados["results"]["bindings"]:
+            productos.append({
+                "producto": r["producto"]["value"],
+                "nombre": r["nombre"]["value"],
+                "sabor": r.get("sabor", {}).get("value", "N/A"),
+                "tipo": r["tipo"]["value"],
+                "idioma": r["lang"]["value"]
+            })
+
+        return productos
+
+    except Exception as e:
+        print("Error al consultar productos tipo cake:", e)
+        return []
